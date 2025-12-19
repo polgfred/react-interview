a0cd6f955d2bf169414f16456854077d25724742
Frederick Polgardy <fred@polgardy.com>
2025-12-19 06:36:19 +0000

    update deps and use mui

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { AppProps } from 'next/app';
-import React from 'react';
-import '../styles.scss';
-
-const RunwayApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
-
-export default RunwayApp;
